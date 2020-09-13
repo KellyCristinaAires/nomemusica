3fad4137f5c03e07b5d6af4238e41ada4c29ada4
kellyCristinaAires <k.woys@hotmail.com>
2020-09-13 14:43:29 -0300

    atividade musica

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace atividade_musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              Console.ForegroundColor = ConsoleColor.Green;
+              Console.WriteLine(" Reluz");
+              Console.Write("\n");
+              Console.ResetColor(); 
+
+              Console.WriteLine("Brillha no céude novo uma estrela ahaa");  
+              Console.WriteLine("Soltando a luz que reluz seu olhar");
+              Console.WriteLine("Paira no tempo um sonho perdido u u u");  
+              Console.WriteLine("Que a gente só pensa em de novo encontrar");  
+              Console.WriteLine("Solto teu grito seu nome ao vento");  
+              Console.WriteLine("E fico voando no meu pensamento");  
+              Console.WriteLine(" Te espero mais livre a cada manhã");
+              Console.Write("\n");
+              
+              Console.ForegroundColor = ConsoleColor.DarkGreen;
+              Console.WriteLine("Cheiro uma flor de cravo e canela");
+              Console.WriteLine("E fico curtindo da minha janela");
+              Console.WriteLine("Teu corpo suave de hortelã");
+              Console.Write("\n");
+              Console.ResetColor();
+              Console.WriteLine("Canção de Marcos SABINO");
+        }
+    }
+}
